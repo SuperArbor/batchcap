48085cf60bb19a7bb9e8c1366453fe69f18a380e
SuperArbor <snrihc219@hotmail.com>
2022-05-17 01:48:51 +0800

    bug fix: arguments of boolean type in argparse should use action='store_true' instead of default=False since 'False' will be treated as a string in shell and be converted to True.

--- batchcap.py
+++ batchcap.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--directory', type=str, default=os.path.dirname(__file__), help='Path of directory or file.')
-    parser.add_argument('-o', '--overwrite', default=False, action='store_true', help='Whether or not overwrite existing files.')
+    parser.add_argument('-o', '--overwrite', action='store_true', help='Whether or not overwrite existing files.')
     parser.add_argument('-s', '--seek', type=float, default=0, help='Time of the first capture.')
     parser.add_argument('-w', '--width', type=int, default=360, help='Width of each image.')
     parser.add_argument('-t', '--tile', type=str, default='3x5', help='Tile shaple of the screen shots.')
