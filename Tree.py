860fc66254ac5421cbba9cc835ae79be0152e3c8
SuperArbor <snrihc219@hotmail.com>
2022-05-18 01:34:54 +0800

    Handle root representation by distinguish between id and abs_id.

--- Tree.py
+++ Tree.py
@@ -1,13 +1,15 @@
 from abc import ABC, abstractmethod
+import os
+SEP='/'
 
 class NodeObject(ABC):
     '''Base class for NodeFile and NodeDir.'''
     def __init__(self, id:str, dir, is_dir=False):
         self.id = id
         if dir:
-            self.abs_id = f"{dir.abs_id if dir.abs_id != '/' else ''}/{id}"
+            self.abs_id = (dir.abs_id if not dir.abs_id.endswith(SEP) else dir.abs_id[:-len(SEP)]) + SEP + id
         else:
-            self.abs_id = '/'
+            self.abs_id = id if id else SEP
         self._is_dir = is_dir
         self.dir = dir
         self.data = None
@@ -26,6 +28,17 @@ class NodeObject(ABC):
         while(cnt.dir):
             cnt = cnt.dir
         return cnt
+    
+    def get_root_id(self):
+        root = self.get_root()
+        return root.id
+    
+    def get_root_abs_id(self):
+        root = self.get_root()
+        return root.abs_id
+    
+    def get_true_path(self, drive:str):
+        return drive + self.abs_id
     
     @abstractmethod
     def is_leaf(self) -> bool:
@@ -83,16 +96,16 @@ class NodeDir(NodeObject):
         for id, n in node.get_elements():
             if filter(n):
                 if not n.is_dir():
-                    prnt += '  ' * depth + '-' + (str(n) if abs_id else id) + '\n'
+                    prnt += '  ' * depth + '- ' + (str(n) if abs_id else id) + '\n'
                 else:
-                    prnt += '  ' * depth + '+' + (str(n) if abs_id else id) + '\n'
+                    prnt += '  ' * depth + '+ ' + (str(n) if abs_id else id) + '\n'
                     prnt = self._ls(n, depth+1, prnt, filter=filter, abs_id=abs_id) + '\n'
 
         return prnt.strip()
 
     def touch(self, path:str):
         '''Create a file under the specified directory.'''
-        sections = path.split('/')
+        sections = path.split(SEP)
         cnt = self
         for sect in sections[:-1]:
             if sect not in cnt.elements.keys():
@@ -105,7 +118,7 @@ class NodeDir(NodeObject):
     
     def mkdir(self, path:str):
         '''Create a sub directory under the specified directory.'''
-        sections = path.split('/')
+        sections = path.split(SEP)
         cnt = self
         for sect in sections:
             if sect not in cnt.elements.keys():
@@ -116,7 +129,7 @@ class NodeDir(NodeObject):
 
     def rm(self, path:str):
         '''Remove a node from the specified directory.'''
-        sections = path.split('/')
+        sections = path.split(SEP)
         cnt = self
         for sect in sections:
             if sect in cnt.elements.keys():
@@ -137,10 +150,10 @@ class NodeDir(NodeObject):
         if not path:
             return self
         
-        sections = path.split('/')
-        if sections[0] == '':
+        sections = path.split(SEP)
+        if sections[0] == self.get_root_id():
             root = self.get_root()
-            return root.cd(path[1:])
+            return root.cd(SEP.join(sections[1:]))
         
         cnt = self
         for sect in sections:
@@ -150,19 +163,24 @@ class NodeDir(NodeObject):
                 raise ValueError(f"Invalid relative path {sect} for directory {cnt}")
         return cnt
     
-    def walk(self) -> list:
+    def walk(self, filter=None) -> list:
         '''Get all the nodes under current directory.'''
+        if not filter:
+            filter = lambda _ : True
+            
         lst = []
         for _, n in self.get_elements():
-            lst.append(n)
+            if filter(n):
+                lst.append(n)
             if n.is_dir():
-                lst.extend(n.walk())
+                lst.extend(n.walk(filter))
 
         return lst
     
     
 if __name__ == "__main__":
     root = NodeDir('', dir=None)
+    print(f"root: {root}")
     a = root.mkdir('a')['a']
     g = a.touch('g')['g']
     h = a.mkdir('h')['h']
@@ -172,20 +190,21 @@ if __name__ == "__main__":
     d = c.touch('d')['d']
     e = c.mkdir('e')['e']
     e.touch('f')
+    e.mkdir('o/p')
     
     filter = lambda n: n.get_data() != 'remove'
     print(f"Before modifying:")
     print(root.ls(path='', filter=filter))
-    print(f"cd to 'a/h'")
-    print(root.cd('a/h'))
+    print(f"cd to f'a{SEP}h'")
+    print(root.cd(f'a{SEP}h'))
     
     print(f"rm a/h from {root}")
-    root.rm('a/h')
+    root.rm(f'a{SEP}h')
     print(f"After modifying:")
-    print(root.ls(path='', filter=filter))
+    print(root.ls(path='', filter=filter, abs_id=True))
     
     print(f"cd to 'c/e' from {c}")
-    print(c.cd('/c/e'))
+    print(c.cd(f'{SEP}c{SEP}e'))
     print(f"ls from c")
     print(c.ls(path='', filter=filter))
     
